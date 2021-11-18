0a0fefa05355cbd02761c26b7cacf1572a874b5c
Shengqi <shengqiw97@gmail.com>
2021-11-18 12:35:39 -0600

    added clash royale node-fetch

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ import fetch from "node-fetch";
 const app = express();
 
 app.get("/", async (req, res) => {
-  const data = await fetch("https://api.publicapis.org/entries").then((r) =>
-    r.json()
-  );
+  const data = await fetch(
+    "https://api.clashroyale.com/v1/players/%23P92LLUR8",
+    {
+      method: "GET",
+      headers: {
+        Authorization:
+          "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImU4ODQ3MjA3LWVkNWQtNGE3NS1iODk5LWUyNjRlNzQzNGFiNyIsImlhdCI6MTYzNzI2MDQ3OCwic3ViIjoiZGV2ZWxvcGVyLzExMTU1OTNkLTgwZDctYThjMS01MTA3LTY1OTQwODIxNDI0MiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIzLjIzMy4xMDUuMzkiXSwidHlwZSI6ImNsaWVudCJ9XX0.RzRMf3r2gKsahE9edXrHhAdcx2mz_GUF165FRh4Zm6ZripyCzIKtINiPmGpKCPgog1-CJ6OqwQb1Zg4FiwNppQ",
+        Accept: "application/json",
+      },
+    }
+  ).then((res) => res.json());
   res.status(200).send(data);
 });
 app.listen(3000, function () {
