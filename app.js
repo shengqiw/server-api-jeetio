d490958aafb2f8274c5f3352d3189eaa3d1e21e0
Shengqi <shengqiw97@gmail.com>
2021-11-18 11:51:11 -0600

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  const data = await fetch("https://api.publicapis.org/entries").then((r) =>
+    r.json()
+  );
+  res.status(200).send(data);
+});
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
